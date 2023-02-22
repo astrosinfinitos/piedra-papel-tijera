a58becc2e9a8a42e4efd2e0e42a2ba41a4128646
Alberto Zarzo <118359197+astrosinfinitos@users.noreply.github.com>
2023-02-22 21:54:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,71 +1,52 @@
-/*
-Desafío: 
-Este código tiene demasiadas líneas. 
-El servidor de producción está petado.
-¿Qué harás para reducir el número de líneas de este programa?
-Aquí, tu compi junior Isma te ha dado una pista.
-https://dmitripavlutin.com/access-object-properties-javascript/
-*/
+import { resetBorder } from "./modules/resetBorder.js";
 
-const opciones = ['rock', 'paper', 'scissors']; /*Cargamos todas las opciones disponibles*/
+const OPCIONES = ["rock", "paper", "scissors"]; /* Cargamos todas las OPCIONES disponibles */
 let playerSelection;
 let cpuSelection;
 
-const [rockButton, paperButton, scissorsButton] = /*Seleccionamos la clase player dentro del elemento button*/
+const [rockButton, paperButton, scissorsButton] = /* Seleccionamos la clase player dentro del elemento button */
     document.querySelectorAll(".player > button");
-const cpuImage = document.querySelector('.cpu img'); /*Con esta línea cargamos la imagen */
-const result = document.querySelector('.result');
+const cpuImage = document.querySelector(".cpu img"); /* Con esta línea cargamos la imagen */
+const result = document.querySelector(".result");
 
-rockButton.addEventListener('click', () => { /*Este evento controla cuando pulsamos la opción piedra*/
-    resetBorder()
-    playerSelection = choosePlayerOption(0);
-    rockButton.classList.add('selected');
-    cpuSelection = chooseCPUOption();
-    show()
-});
+const onButtonPressed = (index, button) => {
+  resetBorder();
+  playerSelection = choosePlayerOption(index);
+  button.classList.add("selected");
+  cpuSelection = chooseCPUOption();
+  checkWinner();
+};
 
-paperButton.addEventListener('click', () => { /*Este evento controla cuando pulsamos la opción papel*/
-    resetBorder()
-    playerSelection = choosePlayerOption(1);
-    paperButton.classList.add('selected');
-    cpuSelection = chooseCPUOption();
-    show()
-});
+/* Estos eventos controlan cuando eliges una funcion */
+rockButton.addEventListener("click", () => onButtonPressed(0, rockButton));
+paperButton.addEventListener("click", () => onButtonPressed(1, paperButton));
+scissorsButton.addEventListener("click", () => onButtonPressed(2, scissorsButton));
 
-scissorsButton.addEventListener('click', () => { /*Este evento controla cuando pulsamos la opción tijeras*/
-    resetBorder()
-    playerSelection = choosePlayerOption(2);
-    scissorsButton.classList.add('selected');
-    cpuSelection = chooseCPUOption();
-    show()
-});
+/* Retornamos la opción que haya escogido el usuario */
+const choosePlayerOption = (index) => OPCIONES[index];
 
-function choosePlayerOption(index){ /*Retornamos la opción que haya escogido el usuario*/
-    return opciones[index];
-}
+/* Esta funcion selecciona la imagen de lo que haya elegido la CPU */
+const chooseCPUOption = () => {
+  const randomIndex = Math.floor(Math.random() * 3);
+  const imageName = OPCIONES[randomIndex];
+  cpuImage.src = "images/" + imageName + ".png";
+  return OPCIONES[randomIndex];
+};
 
-function chooseCPUOption(){ /*Esta funcion selecciona la imagen de lo que haya elegido la CPU*/
-   const randomIndex = Math.floor(Math.random() * 3);
-   const imageName = opciones[randomIndex];
-   cpuImage.src = 'images/'+imageName+ '.png'
-   return opciones[randomIndex];
-}
-
-function show(){ /*Esta función contempla todas las opciones disponibles del juego, pero es muy larga. ¿Puedes reducirla?*/
+/* Esta función contempla todas las OPCIONES disponibles del juego, pero es muy larga. ¿Puedes reducirla? */
+const checkWinner = () => {
     let gameResult; 
+    // Caso de empate
+    if (cpuSelection === playerSelection ) { /* Caso de empate */
+        result.textContent = "Has empatado";
+    } 
     //Caso de empate
     if(cpuSelection === playerSelection){ 
         result.textContent = `Has empatado`;
     } else if(playerSelection == 'scissors' && cpuSelection=='paper' || playerSelection == 'paper' && cpuSelection=='rock' || playerSelection == 'rock' && cpuSelection=='scissors'){
-            result.textContent = `Has ganado`;
+        result.textContent = `Has ganado`;
       }else{
         result.textContent = `Has perdido`;
        }
-}
+};
 
-
-function resetBorder(){ /*Esta función quitará los estilos de los botones */
-    paperButton.classList.remove('selected');
-    scissorsButton.classList.remove('selected');
-    rockButton.classList.remove('selected');
-}
